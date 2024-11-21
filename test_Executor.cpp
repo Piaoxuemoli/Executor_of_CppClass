1d28f0b1a1e6fb42329827563e5618c49662e8d2
zjlhxg <2693781406@qq.com>
2024-11-21 10:33:16 +0800

    should_return_init_pose_when_without_command: Ensures the initial pose is returned in the absence of a command.

--- test_Executor.cpp
+++ test_Executor.cpp
@@ -20,30 +20,4 @@ namespace adas_Executor
 		const Pose target({ 0, 0, 'E' });
 		ASSERT_EQ(target, executor->GetPose());
 	}
-
-	//测试初始化为{1，1，’N’}后，GetPose()返回{1，1，’N’}
-	TEST(ExecutorTest, should_return_seted_pose_when_initialized_with_command)
-	{
-		//given
-		auto executor = std::make_shared<MyExecutor>();
-		executor->IniPose({ 1, 1, 'N' });
-		//when
-		//then
-		const Pose target({ 1, 1, 'N' });
-		ASSERT_EQ(target, executor->GetPose());
-	}
-
-	//测试初始位置为{0，0，’E’}，执行命令M后，GetPose()返回{1，0，’E’}
-	TEST(ExecutorTest, should_return_x_plus_1_given_command_is_M_and_facing_is_E)
-	{
-		// given
-		auto executor = std::make_shared<MyExecutor>();
-		executor->IniPose({ 0, 0, 'E' });
-		// when
-		executor->Execute("M");
-		// then
-		const Pose target({ 1, 0, 'E' });
-		ASSERT_EQ(target, executor->GetPose());
-	}
-
 }
