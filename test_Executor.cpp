0bf6a57b9ab670f7f5bec1a40003b23d54d6dc4b
zjlhxg <2693781406@qq.com>
2024-12-04 11:15:48 +0800

    should_return_x_minus_1_and_heading_S_given_states_0_0_E_and_command_BFL

--- test_Executor.cpp
+++ test_Executor.cpp
@@ -88,4 +88,30 @@ namespace adas_Executor
 		ASSERT_EQ(target_pose, executor.GetPose());
 	}
 
+	//测试状态{0,0,'E'}下执行命令FL后，是否返回正确的下一状态{0,0,'N'}
+	TEST(ExecutorTest, should_return_heading_W_given_states_0_0_E_and_command_FL)
+	{
+		//given
+		MyExecutor executor({ 0, 0, 'N' });
+		//when
+		executor.SetPose({ 0, 0, 'E' });
+		executor.Execute("FL");
+		//then
+		const Pose& target_pose = { 1, 0, 'N' };
+		ASSERT_EQ(target_pose, executor.GetPose());
+	}
+
+	//测试状态{0,0,'E'}下执行命令BFL后，是否返回正确的下一状态{-1,0,'S'}
+	TEST(ExecutorTest, should_return_x_minus_1_and_heading_S_given_states_0_0_E_and_command_BFL)
+	{
+		//given
+		MyExecutor executor({ 0, 0, 'N' });
+		//when
+		executor.SetPose({ 0, 0, 'E' });
+		executor.Execute("BFL");
+		//then
+		const Pose& target_pose = { -1, 0, 'S' };
+		ASSERT_EQ(target_pose, executor.GetPose());
+	}
+
 }
