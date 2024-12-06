6102ba80054abec44fb3418bb2a7751d3f223c43
zjlhxg <2693781406@qq.com>
2024-12-06 15:59:48 +0800

    Bus_should_return_x_minus_1_and_heading_W_given_states_0_0_N_and_command_L

--- test_DifferentCars.cpp
+++ test_DifferentCars.cpp
@@ -40,4 +40,19 @@ namespace adas_Executor
 		EXPECT_EQ(target_pose, executor.GetPose());
 	}
 
+	//测试Bus状态{0,0,'N'}下执行命令L后，是否返回正确的下一状态{-1,0,'W'}
+	TEST(MyExecutorTest, Bus_should_return_x_minus_1_and_heading_W_given_states_0_0_N_and_command_L)
+	{
+		//given
+		MyExecutor executor({ 0, 0, 'N' });
+		//when
+		executor.SetPose({ 0, 0, 'N' });
+		std::string bus = "Bus";
+		executor.SetCar(bus);
+		executor.Execute("L");
+		//then
+		const Pose& target_pose = { 0, 1, 'W' };
+		EXPECT_EQ(target_pose, executor.GetPose());
+	}
+
 }
