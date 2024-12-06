26e57c184fa083e36fb142ae2df309e3644f9568
zjlhxg <2693781406@qq.com>
2024-12-06 15:50:47 +0800

    Roadster_should_return_y_plus_2_given_states_0_0_N_and_command_M

--- test_DifferentCars.cpp
+++ test_DifferentCars.cpp
@@ -17,11 +17,27 @@ namespace adas_Executor
 		MyExecutor executor({ 0, 0, 'N' });
 		//when
 		executor.SetPose({ 0, 0, 'N' });
-		executor.Execute("M");
 		std::string bus = "Bus";
 		executor.SetCar(bus);
+		executor.Execute("M");
 		//then
 		const Pose& target_pose = { 0, 1, 'N' };
 		EXPECT_EQ(target_pose, executor.GetPose());
 	}
+
+	//测试Roadster状态{0,0,'N'}下执行命令M后，是否返回正确的下一状态{0,2,'N'}
+	TEST(MyExecutorTest, Roadster_should_return_y_plus_2_given_states_0_0_N_and_command_M)
+	{
+		//given
+		MyExecutor executor({ 0, 0, 'N' });
+		//when
+		executor.SetPose({ 0, 0, 'N' });
+		std::string roadster = "Roadster";
+		executor.SetCar(roadster);
+		executor.Execute("M");
+		//then
+		const Pose& target_pose = { 0, 2, 'N' };
+		EXPECT_EQ(target_pose, executor.GetPose());
+	}
+
 }
