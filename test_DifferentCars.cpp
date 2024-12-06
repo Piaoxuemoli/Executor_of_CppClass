f300818cfd1f02b4539a6b6c640a52910d55b58e
zjlhxg <2693781406@qq.com>
2024-12-06 16:29:46 +0800

    Bus_should_return_y_minus_1_and_heading_E_given_states_0_0_N_and_command_BL

--- test_DifferentCars.cpp
+++ test_DifferentCars.cpp
@@ -55,4 +55,33 @@ namespace adas_Executor
 		EXPECT_EQ(target_pose, executor.GetPose());
 	}
 
+	//测试Roadster状态{0,0,'N'}下执行命令L后，是否返回正确的下一状态{-1,0,'W'}
+	TEST(MyExecutorTest, Roadster_should_return_x_minus_1_and_heading_W_given_states_0_0_N_and_command_L)
+	{
+		//given
+		MyExecutor executor({ 0, 0, 'N' });
+		//when
+		executor.SetPose({ 0, 0, 'N' });
+		std::string roadster = "Roadster";
+		executor.SetCar(roadster);
+		executor.Execute("L");
+		//then
+		const Pose& target_pose = { -1, 0, 'W' };
+		EXPECT_EQ(target_pose, executor.GetPose());
+	}
+
+	//测试Bus状态{0,0,'N'}下执行命令BL后，是否返回正确的下一状态{0,-1,'E'}
+	TEST(MyExecutorTest, Bus_should_return_y_minus_1_and_heading_E_given_states_0_0_N_and_command_BL)
+	{
+		//given
+		MyExecutor executor({ 0, 0, 'N' });
+		//when
+		executor.SetPose({ 0, 0, 'N' });
+		std::string bus = "Bus";
+		executor.SetCar(bus);
+		executor.Execute("BL");
+		//then
+		const Pose& target_pose = { 0, -1, 'E' };
+		EXPECT_EQ(target_pose, executor.GetPose());
+	}
 }
